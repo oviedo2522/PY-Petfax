62921094cf1c09f8e6cada60d04f692f6d1172dc
Ryan Rasmussen <87948132+RyanJRasmussen@users.noreply.github.com>
2022-07-01 15:02:01 -0500

    Add files via upload

---
+++ PY-petfax-app/petfax/__init__.py
@@ -0,0 +1,32 @@
+# config
+from . import models
+from flask import Flask
+from flask_migrate import Migrate
+
+# factory
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
+
+    # index route
+    @app.route('/')
+    def index():
+        return 'Hello, PetFax!'
+
+    # register pet blueprint
+    from . import pet
+    app.register_blueprint(pet.bp)
+
+    # register fact blueprint
+    from . import fact
+    app.register_blueprint(fact.bp)
+
+    # return the app
+    return app
