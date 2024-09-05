166fa36221f4658ebfb05e510225984543d7bd65
oviedo2522 <oviedo2522@gmail.com>
2024-09-04 20:12:52 -0700

    Finish

--- PY-petfax-app/petfax/__init__.py
+++ PY-petfax-app/petfax/__init__.py
@@ -9,7 +9,7 @@ from flask_migrate import Migrate
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/petfax'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Boogy1527@localhost:5432/petfax'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     models.db.init_app(app)
